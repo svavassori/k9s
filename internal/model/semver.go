724220db3ddffa6fd5a5ef058f4371d211e27d3e
Sergio Vavassori <vavassori@gmail.com>
2024-11-11 17:26:22 +0100

    Change how version is handled to support tags

--- internal/model/semver.go
+++ internal/model/semver.go
@@ -9,34 +9,36 @@ import (
 	"strconv"
 )
 
-var versionRX = regexp.MustCompile(`\Av(\d+)\.(\d+)\.(\d+)\z`)
+var versionRX = regexp.MustCompile(`\Av(\d+)\.(\d+)\.(\d+)(-[a-z]+)?\z`)
 
 // SemVer represents a semantic version.
 type SemVer struct {
 	Major, Minor, Patch int
+	Build               string
 }
 
 // NewSemVer returns a new semantic version.
 func NewSemVer(version string) *SemVer {
 	var v SemVer
-	v.Major, v.Minor, v.Patch = v.parse(NormalizeVersion(version))
+	v.Major, v.Minor, v.Patch, v.Build = v.parse(NormalizeVersion(version))
 
 	return &v
 }
 
 // String returns version as a string.
 func (v *SemVer) String() string {
-	return fmt.Sprintf("v%d.%d.%d", v.Major, v.Minor, v.Patch)
+	return fmt.Sprintf("v%d.%d.%d%s", v.Major, v.Minor, v.Patch, v.Build)
 }
 
-func (*SemVer) parse(version string) (major, minor, patch int) {
+func (*SemVer) parse(version string) (major, minor, patch int, build string) {
 	mm := versionRX.FindStringSubmatch(version)
-	if len(mm) < 4 {
+	if len(mm) < 5 {
 		return
 	}
 	major, _ = strconv.Atoi(mm[1])
 	minor, _ = strconv.Atoi(mm[2])
 	patch, _ = strconv.Atoi(mm[3])
+	build = mm[4]
 
 	return
 }
